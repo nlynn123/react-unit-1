bb73934ffeecf290c1d7a43540850596283461cd
Natalie <hairnatalie7@gmail.com>
2022-10-17 10:44:07 -0600

    commit

---
+++ App.js
@@ -0,0 +1,62 @@
+import logo from './logo.svg';
+import './App.css';
+import Square from "./Square";
+import { useState } from "react"; 
+
+function App() {
+// let propVariable = "labrador"
+  const [squares, setSquares] = useState (["", "", "", "", "", "", "", "", ""]);
+  const [player, setPlayer] = useState(true);
+
+  const handleClick = () => {
+    setSquares = (["", "","","","","","","",""])
+    setPlayer=(true)
+  }
+
+  function calculateWinner (squares) {
+    const lines = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [2,4,6],
+      [0,4,8]
+    ];
+    for (let i=0; i<lines.length; i++){
+      const [a,b,c]=lines[i];
+      if(
+        squares[a] &&
+        squares[a]===arr[b]&&
+        squares[a]===arr[c]
+      ) {
+        return `${squares[a]} won!`;
+      }
+    }
+    return "Who will win?";
+  }
+
+  return (
+    <div className="App">
+      <span>{calculateWinner(squares)}</span>
+      <div className = "container">
+        {squares.map((val, index) => {
+          return (
+         <Square 
+         index={index}
+         squareValue={val}
+        squares={squares}
+        setSquares={setSquares}
+        player = {player}
+       setPlayer={setPlayer}
+      /> 
+          );
+        })}
+      </div>
+      <button onClick={handleClick}>Reset</button>
+    </div>
+  );
+      }
+
+export default App;
